2fa3b3a123ad1902ba075b78b62cd8162e512c9d
Josh Burdick <jburdick@keyfitz.org>
2022-06-09 00:34:51 -0400

    Plots some edges.

--- countingBound/py/figure/zeroing.py
+++ countingBound/py/figure/zeroing.py
@@ -5,7 +5,7 @@ import pdb
 
 import matplotlib
 import matplotlib.pyplot as plt
-import numpy
+import numpy as np
 
 
 
@@ -27,51 +27,44 @@ class CliqueFigure:
     in each plot.
     """
 
-    def __init__(self, plt, n, colors, vertex_0_theta):
+    def __init__(self, n, colors, vertex_0_theta):
         """
-        plt: pyplot object to plot on
         n: number of vertices in each graph
         colors: hash from sets (defined as sorted lists of k numbers)
             to color of each set
         vertex_0_theta: angle at which to place vertex 0
         """
-        self.plt = plt
         self.n = n
         self.colors = colors
         self.alpha = 0.5
         # set the vertices
         theta = np.linspace(0, 2*np.pi, n, endpoint=False) + vertex_0_theta
-        self.vertex = np.stack(np.cos(theta), np.sin(theta))
+        self.vertex = np.stack([np.cos(theta), np.sin(theta)])
 
     def plot_sets(self, radius, center, sets):
         """Plots some sets.
 
         radius: the radius for the sets
         center: the center, as a 2-element numpy array
-        sets: the sets, as a list of k-element lists of numbers
+        sets: the sets, as a list of k-element tuples of ints
         Side effects: plots the sets
         """
         for s in sets:
             v = radius * self.vertex[:,s] + np.array([center]).T
-            plt.fill([1,2,3,1], [2,3,1,2], facecolor=self.colors[s], alpha=self.alpha)
+            plt.fill(v[0,:], v[1,:], facecolor=self.colors[s], alpha=self.alpha)
 
 
-    plt.savefig('zeroing.png')
 
 
 
 
 def plot_it():
     plt.figure()
-    # pdb.set_trace()
-
-    plt.fill([1,2,3,1], [2,3,1,2], c='#a00000', alpha=0.4)
-    plt.savefig('zeroing.png')
-
-
-
+    colors = {(0,1,2):'red', (0,1,3):'orange', (0,2,3):'green', (1,2,3):'blue'}
+    cf = CliqueFigure(4, colors, 0)
+    cf.plot_sets(1, np.array([0,0.1]), [(0,1,2), (0,2,3), (1,2,3)])
+    plt.savefig('test0.png')
 
 if __name__ == '__main__':
-    zf = ZeroingFigure(5)
-
+    plot_it()
 

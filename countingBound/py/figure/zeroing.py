39f134e415f716323a7de17c91a30dd91b86a2bf
Josh Burdick <jburdick@keyfitz.org>
2022-06-11 16:21:57 -0400

    Plots curved lines

--- countingBound/py/figure/zeroing.py
+++ countingBound/py/figure/zeroing.py
@@ -8,7 +8,7 @@ import pdb
 import matplotlib
 import matplotlib.pyplot as plt
 import numpy as np
-
+import scipy.interpolate
 
 
 def scale_lightness(rgb, scale_l):
@@ -56,15 +56,49 @@ class CliqueFigure:
             return
         for s in cliques:
             v = radius * self.vertex[:,s] + np.array([center]).T
-            # pdb.set_trace()
             plt.fill(v[0,:], v[1,:],
                 edgecolor=self.colors[s],
                 facecolor=scale_lightness(self.colors[s], 2),
                 lw=3,
                 alpha=self.alpha)
 
+def interpolate(nodes, num_points=101):
+    """Does spline interpretation.
 
+    Essentially taken from
+    https://stackoverflow.com/questions/29837854/matplotlib-draw-spline-from-multiple-points
+    nodes: points to interpolate between (as a numpy array,
+        with one row per point)
+    num_points: number of interpolated points to include
+    Returns: a numpy array with that many rows
+    """
+    x = nodes[:,0]
+    y = nodes[:,1]
+    tck,u = scipy.interpolate.splprep([x,y], k=2)
+    xnew, ynew = scipy.interpolate.splev(np.linspace(0, 1,num_points), tck,der = 0)
+    return np.stack([xnew, ynew], axis=1)
 
+def curved_line(endpoints):
+    """Draws a curved line between the endpoints."""
+    p = np.array([
+        endpoints[0,:],
+        # in the middle is the midpoint, except a bit higher up
+        np.mean(endpoints, axis=0) + (0, 0.8),
+        endpoints[1,:] ])
+    return interpolate(p)
+
+def zero_out_edges(cliques):
+    """
+    Finds the cliques remaining, after zeroing out an edge.
+
+    """
+    # get the vertices which are relevant
+    vertices = set(list(itertools.chain(*cliques)))
+    # the edges, which we could zero out
+    edges = itertools.combinations(vertices, 2)
+    def cliques_remaining(edge):
+        return tuple([c for c in cliques if not set(edge) < set(c)])
+    return list(tuple([cliques_remaining(e) for e in edges]))
 
 def plot_Z_relation():
     """Plots the 'zeroing-one-edge' relation."""
@@ -85,17 +119,23 @@ def plot_Z_relation():
     all_edges = list(itertools.combinations(range(4), 3))
     for j in range(0, 5):
         print(j)
+        # ??? should this be a set rather than a tuple?
         subsets = tuple(itertools.combinations(all_edges, j))
         for i in range(len(subsets)):
             print(((i,j), subsets[i]))
             set_location[subsets[i]] = (i + 1/2 - len(subsets)/2, j)
     # plot effects of zeroing out an edge
+    for (cliques, location) in set_location.items():
+        cliques_below = zero_out_edges(cliques)
+        for c in cliques_below:
+            location_1 = set_location[c]
+            p = curved_line(np.array([location, location_1]))
+            plt.plot(p[:,0], p[:,1], c='black', alpha=0.3)
 
-
-
+ 
     # plot the sets
-    for (edges, location) in set_location.items():
-        cf.plot_cliques(0.25, location, edges)
+    for (cliques, location) in set_location.items():
+        cf.plot_cliques(0.25, location, cliques)
     # cf.plot_sets(0.4, np.array([0,0.1]), [(0,1,2), (0,1,3)])
     plt.savefig('Z.png')
 

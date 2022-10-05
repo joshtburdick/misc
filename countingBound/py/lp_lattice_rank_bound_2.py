7a55fc880e47b4c9cb067d3ebb35a5902b07ea47
Josh Burdick <josh.t.burdick@gmail.com>
2022-10-05 14:00:44 -0400

    Sketch revised lattice bound

---
+++ countingBound/py/lp_lattice_rank_bound_2.py
@@ -0,0 +1,21 @@
+
+
+
+
+
+
+class LatticeRankBound2:
+    """Bound, using the lattice of canonical hypergraphs.
+
+
+
+    """
+
+    def __init__(self, n, k):
+
+        self.graph_info = FIXME
+
+        self.lp =   (self.graph_info.canonical_graphs)
+
+
+

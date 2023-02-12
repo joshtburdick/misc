29a0f1df6141abd7879b35d2ee3620d3c9e3d773
Josh Burdick <jburdick@keyfitz.org>
2023-02-12 11:32:13 -0500

    Disable regenerating some figures in R
    (Using make would also fix this.)

--- countingBound/make.sh
+++ countingBound/make.sh
@@ -1,9 +1,13 @@
 #!/bin/bash
 # Builds the document.
 
-(cd R; R CMD BATCH --no-save --quiet figs1.R )
+# omitting this for now, since these keep re-running,
+# and showing up in the git diffs
+# (these may get rewritten in Python anyway)
+# (cd R; R CMD BATCH --no-save --quiet figs1.R )
+
 (cd py/figure/; ./zeroing.py)
-(cd py/; ./lp_brute_force_1.py 6 3 1 > lp_brute_force_1_6_3.txt)
+# (cd py/; ./lp_brute_force_1.py 6 3 1 > lp_brute_force_1_6_3.txt)
 # (cd py; ./approxRank.py)
 pdflatex countingBound.tex
 bibtex countingBound.aux

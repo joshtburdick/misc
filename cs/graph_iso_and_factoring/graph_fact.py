ae7b736d5199085e6b4d0aadd7c05467920cc20e
Josh Burdick <josh.t.burdick@gmail.com>
2023-04-28 22:55:02 -0400

    This doesn't work.

--- cs/graph_iso_and_factoring/graph_fact.py
+++ cs/graph_iso_and_factoring/graph_fact.py
@@ -4,7 +4,9 @@
 # write a program to check some examples, rather than drawing
 # graphs with pencil and paper.)
 
+import itertools
 import pdb
+import sys
 
 import networkx
 import networkx.algorithms.isomorphism.vf2pp
@@ -21,7 +23,8 @@ class GraphIsoFactor:
         """
         self.primes = primes
         # precompute graph for 1
-        self.g1 = self.get_graph(1)
+        # Deprecated: this doesn't seem useful after all.
+        # self.g1 = self.get_graph(1)
 
     def get_graph(self, x):
         """Makes a graph for x.
@@ -30,13 +33,20 @@ class GraphIsoFactor:
         """
         # initialize the graph
         g = networkx.Graph()
+        # add "end" edges
+        # first, loop through the "shared edges"
+        if True:
+          for p in self.primes:
+            # then loop through the remaining pairs of primes
+            primes1 = list(set(self.primes) - set([p]))
+            for (p1, p2) in itertools.combinations(primes1, 2):
+                self.add_end_CBIP(g, p, p1, p2, 'A')
+                self.add_end_CBIP(g, p, p1, p2, 'B')
+        # add "middle" edges
         for i in range(len(self.primes)-1):
             for j in range(i+1, len(self.primes)):
                 p1 = self.primes[i]
                 p2 = self.primes[j]
-                # add edges at each "end"
-                self.add_end(g, p1, p2, 'A')
-                self.add_end(g, p1, p2, 'B')
                 # add "middle" edges
                 self.add_middle(g, p1, p2, x)
         return g
@@ -44,6 +54,7 @@ class GraphIsoFactor:
     def add_end(self, g, p1, p2, end):
         """Adds on 'end-to-middle' edges.
 
+        Deprecated.
         g: the graph to add to
         p1, p2: the primes to add an "end" for
         end: the "end" to add on (either 'A' or 'B')
@@ -57,18 +68,24 @@ class GraphIsoFactor:
                 g.add_edge((end, p1, x1), (end, p1, p2, i))
                 g.add_edge((end, p2, x2), (end, p1, p2, i))
 
-    def add_end_CBIP(self, g, p1, p2, end):
+    def add_end_CBIP(self, g, p, p1, p2, end):
         """Adds on a CBIP of end edges.
 
         g: the graph to add to
-        p, p1, p2: the primes to add an "end" for
+        p: the prime that these have in common
+        p1, p2: the other two primes to add an "end" for
         end: the "end" to add on (either 'A' or 'B')
         Side effects: adds a CBIP of edges to g, of vertices
             which are equivalent mod p.
         """
-        pass
-        # FIXME
-
+        def sort_p(end, p1, p2, x):
+            """Utility to put the primes in order."""
+            return (end, min(p1, p2), max(p1, p2), x)
+        for i in range(1, p):
+            for x1 in range(1, p1):
+                for x2 in range(1, p2):
+                    g.add_edge(sort_p(end, p, p1, x1*p+i),
+                        sort_p(end, p, p2, x2*p+i))
 
     def add_middle(self, g, p1, p2, x):
         """Adds the 'middle' connections.
@@ -102,26 +119,24 @@ class GraphIsoFactor:
                 print(f'{y} (mod {p})')
         print('------')
 
-    def print_isos(self, x):
+    def print_isos(self, x1, x2):
         """Print all the isomorphisms."""
-        g = gif.get_graph(x)
+        g1 = gif.get_graph(x1)
+        g2 = gif.get_graph(x2)
+        # pdb.set_trace()
         count = 0
-        for iso in networkx.algorithms.isomorphism.vf2pp.vf2pp_all_isomorphisms(self.g1, g):
-            # for now, just printing counts
+        for iso in networkx.algorithms.isomorphism.vf2pp.vf2pp_all_isomorphisms(g1, g2):
+            # for now, just counting these
             count += 1
-#            if count % 1 == 0:
-#                print(count)
+            if count % 10 == 0:
+                print(count)
         print(count)
             # self.print_factors_from_iso(iso)
             # print()
 
 if __name__ == '__main__':
-    gif = GraphIsoFactor([3,7])
-    gif.print_isos(10)
-
-# factors   # isos
-# 3,5       96 = 2^5 * 3 = 4 * 4!
-# 3,7       2880 = 2^6 * 3^2 * 14 = 4! * 5!
-# 5,7       dunno, it's slooooow...
-# summary: don't know what, if any, pattern this is
+    gif = GraphIsoFactor([5,7,11])
+    x1 = int(sys.argv[1])
+    x2 = int(sys.argv[2])
+    gif.print_isos(x1, x2)
 

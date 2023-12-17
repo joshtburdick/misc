0e30eb1e3ac40c5059c7ed96aecef45b5e3c4ebd
Josh Burdick <josh.t.burdick@gmail.com>
2023-12-17 00:19:17 -0500

    Sketch clique gadget at ends

--- cs/graph_iso_and_factoring/graph_fact.py
+++ cs/graph_iso_and_factoring/graph_fact.py
@@ -21,7 +21,7 @@ class GraphIsoFactor:
 
         primes: list of primes to use
         """
-        self.primes = primes
+        self.primes = sorted(primes)
         # precompute graph for 1
         # Deprecated: this doesn't seem useful after all.
         # self.g1 = self.get_graph(1)
@@ -43,18 +43,14 @@ class GraphIsoFactor:
                 self.add_end_CBIP(g, p, p1, p2, 'A')
                 self.add_end_CBIP(g, p, p1, p2, 'B')
         # add "middle" edges
-        for i in range(len(self.primes)-1):
-            for j in range(i+1, len(self.primes)):
-                p1 = self.primes[i]
-                p2 = self.primes[j]
-                # add "middle" edges
-                self.add_middle(g, p1, p2, x)
+        for (p1, p2) in itertools.combinations(primes1, 2):
+            self.add_middle(g, p1, p2, x)
         return g
 
     def add_end(self, g, p1, p2, end):
         """Adds on 'end-to-middle' edges.
 
-        Deprecated.
+        Deprecated: I don't think this gadget works.
         g: the graph to add to
         p1, p2: the primes to add an "end" for
         end: the "end" to add on (either 'A' or 'B')
@@ -70,6 +66,26 @@ class GraphIsoFactor:
 
     def add_end_CBIP(self, g, p, p1, p2, end):
         """Adds on a CBIP of end edges.
+        
+        Deprecated.
+        g: the graph to add to
+        p: the prime that these have in common
+        p1, p2: the other two primes to add an "end" for
+        end: the "end" to add on (either 'A' or 'B')
+        Side effects: adds a CBIP of edges to g, of vertices
+            which are equivalent mod p.
+        """
+        def sort_p(end, p1, p2, x):
+            """Utility to put the primes in order."""
+            return (end, min(p1, p2), max(p1, p2), x)
+        for i in range(1, p):
+            for x1 in range(1, p1):
+                for x2 in range(1, p2):
+                    g.add_edge(sort_p(end, p, p1, x1*p+i),
+                        sort_p(end, p, p2, x2*p+i))
+
+    def add_end_clique(self, g, p, p1, p2, end):
+        """Adds on a clique of end edges.
 
         g: the graph to add to
         p: the prime that these have in common
@@ -86,6 +102,33 @@ class GraphIsoFactor:
                 for x2 in range(1, p2):
                     g.add_edge(sort_p(end, p, p1, x1*p+i),
                         sort_p(end, p, p2, x2*p+i))
+
+    def add_end(self, end):
+        """Adds 'end' connections to A and B."""
+        for p in self.primes:
+            for x in range(1, p):
+                g.add_edge(end, (end, p, x))
+
+    def add_grid(self, end, p1, p2):
+        """Adds 'grid' of connections for a pair of primes."""
+        def add_clique(vertices):
+            """Utility to add a clique of edges."""
+            for pair in itertools.combinations(vertices, 2):
+                g.add_edge(pair[0], pair[1])
+        # first, construct a multiplication table
+        m = p1 * p2
+        x = np.full([p1, p2], np.nan)
+        for i in range(p1):
+            for j in range(p2):
+                x[i, j] = (i * j) % m
+        # add rows
+        for i in range(p1):
+            add_clique([end, p1, p2, y) for y in x[i,:]]
+                + [(end, p1, i)])
+        # add columns
+        for j in range(p2):
+            add_clique([end, p1, p2, y) for y in x[:,j]]
+                + [(end, p2, j)])
 
     def add_middle(self, g, p1, p2, x):
         """Adds the 'middle' connections.
@@ -106,7 +149,7 @@ class GraphIsoFactor:
             for b in range(1, m):
                 # XXX not worrying much about efficiency here
                 # we assumed x1 is relatively prime to p1 and p2, so
-                # I think we don't need to check that here
+                # I don't think we need to check that here
                 if a*b % m == x1:
                     g.add_edge(('A', p1, p2, a), ('B', p1, p2, b))
 
@@ -135,7 +178,8 @@ class GraphIsoFactor:
             # print()
 
 if __name__ == '__main__':
-    gif = GraphIsoFactor([5,7,11])
+    # gif = GraphIsoFactor([5,7,11])
+    gif = GraphIsoFactor([3, 5, 7])
     x1 = int(sys.argv[1])
     x2 = int(sys.argv[2])
     gif.print_isos(x1, x2)

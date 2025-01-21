b21e973d28a4488a53cb8bd95036d18dd8056fdf
Josh Burdick <josh.t.burdick@gmail.com>
2025-01-20 23:22:21 -0500

    Sketch "windows" of bounds

--- countingBound/py/figure/walking_bounds.py
+++ countingBound/py/figure/walking_bounds.py
@@ -32,23 +32,37 @@ class BouncePlot:
         U_t0: starting point (x, y) = (num. cliques, num. gates)
         Returns: a dict with keys "U_t0", "V_t1", and "U_t1".
             U_t2 and V_t2 will be numpy arrays with columns "x", "y_lo", and "y_hi".
+            ... also, "V_t1_bounds" and "U_t2_bounds", which are endpoints of
+                polygons indicating the bounds.
         """
+        U_t0 = np.array(U_t0)
         # bounce "down"
         num_hit = self.rng.hypergeometric(U_t0[0], self.N - U_t0[0], self.h, num_samples)
         V_t1_x = U_t0[0] - num_hit
         V_t1_y_lo = U_t0[1] - num_hit
         V_t1_y_hi = np.full(num_samples, [U_t0[1]])
-        
+
+        max_h = min(U_t0[0], self.h)
+        # note that we duplicate the last point, to streamline drawing connecting lines
+        V_t1_bounds = np.stack([U_t0 - (max_h, max_h), U_t0 - (max_h, 0), U_t0, U_t0])
+
         # bounce "up"
         num_added = self.rng.binomial(self.h, 0.5, num_samples)
         U_t2_x = V_t1_x + num_added
         U_t2_y_lo = V_t1_y_lo
         U_t2_y_hi = V_t1_y_hi + num_added
+        U_t2_bounds = np.stack([
+            V_t1_bounds[0,:],
+            V_t1_bounds[1,:],
+            V_t1_bounds[1,:] + (self.h, self.h),
+            V_t1_bounds[0,:] + (self.h, 0)])
 
         return {
             "U_t0": U_t0,
             "V_t1": np.stack([V_t1_x, V_t1_y_lo, V_t1_y_hi], axis=1),
             "U_t2": np.stack([U_t2_x, U_t2_y_lo, U_t2_y_hi], axis=1),
+            "V_t1_bounds": V_t1_bounds,
+            "U_t2_bounds": U_t2_bounds
         }
 
     def plot_lines(self, axs, p, hue, alpha=0.4):

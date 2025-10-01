d6506defb6ec0efac19255279d955217e691f074
Josh Burdick <josh.t.burdick@gmail.com>
2025-10-01 11:57:54 -0400

    Print a summary of the max

--- countingBound/py/fractions/band/band_bound.py
+++ countingBound/py/fractions/band/band_bound.py
@@ -8,6 +8,7 @@ import math
 import pdb
 import sys
 
+import matplotlib as plt
 import numpy as np
 import pandas
 import scipy.special
@@ -25,20 +26,20 @@ def band_bound(n, k, max_band_width):
     """Computes the 'band bound'.
 
     n, k: Problem size.
-    max_band_width: when bounding level i, for j in [0,max_band_width),
-        group together levels i..i+j.
+    max_band_width: when bounding level i, for w in [0,max_band_width),
+        group together levels i..i+w.
     Returns: an array of shape [max_band_width, comb(n, k)], of
         lower bounds.
     """
     basis = gate_basis.UnboundedFanInNandBasis()
     num_inputs = comb(n, 2)
     N = comb(n, k)
-    b = np.zeros([max_band_width, N], dtype=int)
-    num_functions = comb(N, range(N+1))
-    for i in range(N+1):
-        for j in range(max_band_width):
-            total_functions = sum(num_functions[i:i+j])
-            b[i,j] = max(0, basis.expected_gates(num_inputs, log2(total_functions)) - j)
+    b = np.zeros([max_band_width, N+1])
+    num_functions = [comb(N, i) for i in range(N+1)]
+    for j in range(N+1):
+        for w in range(max_band_width):
+            total_functions = sum(num_functions[j:j+w+1])
+            b[w,j] = max(0, basis.expected_gates(num_inputs, math.log2(total_functions)) - w)
     return b
 
 def parse_args():
@@ -51,13 +52,10 @@ def parse_args():
         help="Size of clique")
     parser.add_argument("max_band_width", type=int,
         help="Maximum 'band width' to consider")
-   return parser.parse_args()
+    return parser.parse_args()
 
 if __name__ == '__main__':
     args = parse_args()
     b = band_bound(args.n, args.k, args.max_band_width)
-
-
-
-
+    print(np.max(b, axis=1))
 
